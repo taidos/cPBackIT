8ab6337e85cd5d134d651971f2c859a6169a2ba3
Carlos Faustino <70132833+taidos@users.noreply.github.com>
2021-06-30 17:17:04 +0100

    Update landing.php

--- files/whm/landing.php
+++ files/whm/landing.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> <html 
-xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> <head> <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> <link 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> <html
+xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> <head> <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> <link
 rel="stylesheet" type="text/css" href="style.css"></link> </head> <body> <div class="headcontent">
-        <a href="index.php">cPBackIT - cPanel Remote Backup</a> </div> <div class="content"> <p> <font size='3'> <a href="conf_bkp.php">Check Backup 
-Configuration</a> &nbsp;&nbsp;&nbsp; <a href="about.php">About</a> &nbsp;&nbsp;&nbsp; </font> </p>
+        <a href="index.php">cPBackIT - cPanel Remote Backup</a> </div> <div class="content"> <p> <font size='3'> <a href="conf_bkp.php">Check Backup
+Configuration</a> &nbsp;&nbsp;&nbsp; <a href="restore/index.php">Restore one Account</a> &nbsp;&nbsp;&nbsp; <a href="about.php">About</a> &nbsp;&nbsp;&nbsp; </font> </p>
