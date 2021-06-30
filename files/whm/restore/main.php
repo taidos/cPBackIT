a831e4883175a8a5b4a62f908d155fd10ce251a7
Carlos Faustino <70132833+taidos@users.noreply.github.com>
2021-06-30 17:15:52 +0100

    Create main.php

---
+++ files/whm/restore/main.php
@@ -0,0 +1,27 @@
+<?php
+#!/usr/bin/php-cgi
+
+$usario = $_ENV['REMOTE_USER'];
+//$usario = "carlosfaustino";
+$lines = file("/home/".$usario."/.cpbackit/cpbackit.conf");
+
+foreach ($lines as $key) {
+$term = explode("=",trim($key));
+        if($term[0]=='HOST'){
+                        $key_remote_ip=$term[1];
+                }
+        elseif( $term[0]=='USER') {
+                        $key_remote_user=$term[1];
+                }
+        elseif($term[0]=='PASSWD' ) {
+                        $key_remote_port=$term[1];
+                }
+        elseif($term[0]=='FOLDER' ) {
+                        $key_restination_folder=$term[1];
+                }
+        elseif($term[0]=='EMAIL' ) {
+                        $key_email=$term[1];
+                }
+
+}
+?>
